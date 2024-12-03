24a45a6050ae7f1b38de281421cae0cef5fdaed0
seekwallity <aaron.d.johnson13@gmail.com>
2024-12-02 23:13:08 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import StreamList from './components/StreamList';
+import Movies from './components/Movies';
+import Cart from './components/Cart';
+import About from './components/About';
+import SearchPage from "./components/SearchPage";
+import MovieResults from "./components/MovieResults";
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<StreamList />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<SearchPage />} />
+          <Route path="/results" element={<MovieResults />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
